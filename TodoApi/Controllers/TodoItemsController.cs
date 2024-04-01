80f8df28082cafe2b6881c9a9593914b54a91fb1
Bugmenn <gabrielbvanzuita@gmail.com>
2024-04-01 15:12:39 -0300

    modified:   TodoApi/Controllers/TodoItemsController.cs 	modified:   TodoApi/bin/Debug/net8.0/TodoApi.dll 	modified:   TodoApi/bin/Debug/net8.0/TodoApi.exe 	modified:   TodoApi/bin/Debug/net8.0/TodoApi.pdb 	modified:   TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs 	modified:   TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfoInputs.cache 	modified:   TodoApi/obj/Debug/net8.0/TodoApi.dll 	modified:   TodoApi/obj/Debug/net8.0/TodoApi.pdb 	modified:   TodoApi/obj/Debug/net8.0/TodoApi.sourcelink.json 	modified:   TodoApi/obj/Debug/net8.0/apphost.exe 	modified:   TodoApi/obj/Debug/net8.0/ref/TodoApi.dll 	modified:   TodoApi/obj/Debug/net8.0/refint/TodoApi.dll

--- TodoApi/Controllers/TodoItemsController.cs
+++ TodoApi/Controllers/TodoItemsController.cs
@@ -9,6 +9,10 @@ using TodoApi.Models;
 
 namespace TodoApi.Controllers
 {
+    // Substitua [controller] pelo nome do controlador, que é o nome de classe do controlador
+    // menos o sufixo "Controlador" por convenção. Para esta amostra, o nome da classe do controlador é
+    // TodoItemsController. Portanto, o nome do controlador é "TodoItems".
+    // O roteamento do ASP.NET Core não diferencia maiúsculas de minúsculas.
     [Route("api/[controller]")]
     [ApiController]
     public class TodoItemsController : ControllerBase
@@ -19,7 +23,17 @@ namespace TodoApi.Controllers
         {
             _context = context;
         }
+        // O tipo de retorno dos metodos GET é tipo <ActionResult>, podendo representar uma ampla variedade de códigos de status HTTP.
+        // Sendo estes: 1° se nenhum item corresponder à ID solicitada, o método retornará um código de erro Status 404NotFound e
+        // Caso contrário, o método retornará 200 com um corpo de resposta JSON. O retorno de item resulta em uma resposta HTTP 200.
+        // O ASP.NET Core serializa automaticamente o objeto em JSON e grava o JSON no corpo da mensagem de resposta.
+        // O código de resposta para esse tipo de retorno é 200 OK, supondo que não haja nenhuma exceção sem tratamento.
+        // As exceções sem tratamento são convertidas em erros 5xx.
 
+        // O atributo [HttpGet] indica um método que responde a uma solicitação HTTP GET.
+        // O caminho da URL de cada método é construído da seguinte maneira.
+        // Se o atributo [HttpGet] tiver um modelo de rota (por exemplo, [HttpGet("products")]), acrescente isso ao caminho.
+        // Porém esta não usa.
         // GET: api/TodoItems
         [HttpGet]
         public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
@@ -27,6 +41,8 @@ namespace TodoApi.Controllers
             return await _context.TodoItems.ToListAsync();
         }
 
+        // No método GetTodoItem a seguir, "{id}" é uma variável de espaço reservado para o identificador exclusivo do item pendente.
+        // Quando GetTodoItem é invocado, o valor de "{id}" na URL é fornecido para o método no parâmetro id.
         // GET: api/TodoItems/5
         [HttpGet("{id}")]
         public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
@@ -41,8 +57,12 @@ namespace TodoApi.Controllers
             return todoItem;
         }
 
+        // PutTodoItem é semelhante a PostTodoItem, exceto pelo uso de HTTP PUT.
+        // A resposta é 204 (Sem conteúdo). De acordo com a especificação de HTTP, uma solicitação PUT
+        // exige que o cliente envie a entidade inteira atualizada, não apenas as alterações.
+        // Para dar suporte a atualizações parciais, use HTTP PATCH.
+        // Devera haver algum item dentro do banco para realizar está ação.
         // PUT: api/TodoItems/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
         public async Task<IActionResult> PutTodoItem(long id, TodoItem todoItem)
         {
@@ -94,6 +114,8 @@ namespace TodoApi.Controllers
             // evitar o hard-coding do nome da ação, na chamada CreatedAtAction.
         }
 
+        // Exclui o TodoItem que tem o ID especificado.
+        // Se não existir, a resposta é HTTP 204 No Content
         // DELETE: api/TodoItems/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteTodoItem(long id)
