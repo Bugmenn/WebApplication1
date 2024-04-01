b2f96d4fc90e11fbb4f0107cfb6673800f0f38f7
Bugmenn <gabrielbvanzuita@gmail.com>
2024-04-01 15:18:22 -0300

    renamed:    "TodoApi/Controllers/anota\303\247\303\265es.md" -> "TodoApi/Controllers/anota\303\247\303\265es.txt" 	modified:   TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs 	modified:   TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfoInputs.cache

--- TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f00f451cd16e8ff4da1dafa6797073263a88259")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fb73b4cac4fd7a527f7bf1b00290cd6d2dbd2af")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
